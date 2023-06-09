396fd565a4a35d74f0e7c38f022b1f61e5ce6510
MalinStal <malinstal.94@gmail.com>
2023-06-09 12:18:26 +0200

    Co-authored-by: Pjohanssonswe <Pjohanssonswe@users.noreply.github.com> Co-authored-by: IsakGunnardo <IsakGunnardo@users.noreply.github.com> Co-authored-by: Simon-Eriksson <Simon-Eriksson@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const header = document.getElementById("header")
+//////// DOM VARIABLER  ////////
+
+const header = document.getElementById("header");
 const container = document.getElementById("container");
 const randomBtn = document.getElementById("randomBtn");
 const searchBtn = document.getElementById("searchBtn");
@@ -6,8 +8,11 @@ const homeBtn = document.getElementById("homeBtn");
 const aboutUs = document.getElementById("aboutUs");
 const contact = document.getElementById("contact");
 const cocktailDiv = document.getElementById("cocktailDiv");
+const form = document.getElementById("form");
+var x = window.matchMedia("(max-width: 1500px)")
 
-//Skapar modal för about us och contact
+////////  VARIABLER FOR MODAL   ////////
+
 let aboutModal = document.getElementById("about-us-div");
 let aboutUsBtn = document.getElementById("aboutUs");
 let contactModal = document.getElementById("contact-div");
@@ -16,104 +21,93 @@ let closeAboutUs = document.getElementById("closeaboutus");
 let closeContact = document.getElementById("closecontact");
 
 
-// Skapar scroll funktionen
+//////// VARIABEL FOR SCROLL BUTTON ////////
+
 let scrollBtn = document.getElementById("scrollBtn");
 
 
-
-
-
-// variabel för antalet sökningar på drinkar
+//////// VARIABEL DRINK COUNT ////////
 let timesSearched = 0;
 
+//////// VARIABLER FOR HAMBURGER MENU ////////
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".navMenu");
 
+//////// VARIABLER TO FETCH URL ////////
 const urlRandom = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 const urlName = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
+//////// INITIATION FUNCTIONS     ////////
 
-//////// menu ///////////
+HomePageCoctails()
+scrollInitiation()
+
+
+//////// EVENT HAMBURGER ///////////
 hamburger.addEventListener("click", e => {
   hamburger.classList.toggle("active")
   navMenu.classList.toggle("active")
 })
 
-document.querySelectorAll(".navLink").forEach(n => n.addEventListener( "click", () => {
+document.querySelectorAll(".navLink").forEach(n => n.addEventListener("click", () => {
   hamburger.classList.remove("active")
   navMenu.classList.remove("active")
 }))
 
-/////////// INITIATION FUNCTIONS     //////////////
 
-HomePageCoctails()
-scrollInitiation()
-
-////// EVENTS ////////
+//////// EVENT SEARCH BUTTON ////////
 
 searchBtn.addEventListener("click", e => {
   e.preventDefault()
   cleanSlate(container)
- 
-  let form = document.createElement("form")
-  header.append(form)
-  //få input fältet i headern att bara komma upp en gång och försvina när man klickar på annan knapp i menyn
-  
-  let searchInput = document.createElement("input")
-  searchInput.type = "text"; 
-  searchInput.placeholder = "Mojito... Martini... etc";
-  let searchDrinkBtn = document.createElement("button")
-  searchDrinkBtn.innerText= "Search Cocktail"
-  searchDrinkBtn.classList.add("drinkBtn")
+  cleanSlate(form)
+  searchForm()
 
-  form.append(searchInput, searchDrinkBtn)
-  //cleanSlate fungerar inte med coctailDiv för att få bort andra drinkar vid ny sökning
-  searchDrinkBtn.addEventListener("click", e => {
-    e.preventDefault()
-    cleanSlate(container)
-    if (searchInput.value !== "") {
-    getCocktailByName(searchInput.value);
-  } 
-  /*else{
-    let article = document.createElement("article")
-    cocktailDiv.append(article)
-    
-    let information = document.createElement("h3")
-    drinkName.innerHTML = "Serch for a drink"
- 
-}*/
-// removes defult drink GG
-searchInput.value = "";
-searchInput.innerHTML = "";
-  
-// after 9 drinks the user is connected to the AA meeting page
-  timesSearched++;
-  if (timesSearched > 9) {
-    window.location.href = "https://alcoholics-anonymous.eu/meetings/?tsml-region=sweden";
-  } 
-  
-});
 })
-// get a random coctail
+
+
+//////// EVENT RANDOM BUTTON, GET A RANDOM COCKTAIL //////// 
+
 randomBtn.addEventListener("click", e => {
   cleanSlate(container)
+  cleanSlate(form)
   getCocktailRandom()
-  //skapar en till get a random coctail knapp som ska vissas när hamburgermenyn är öppen 
-   let randomBtn2 = document.createElement("button")
-  //randomBtn2.classList.add("randomBtn2")
-  header.append(randomBtn2)
- randomBtn2.addEventListener("click", () => {
-   cleanSlate(container)
-    getCocktailRandom()});
-    
+  if (x.matches) {
+    randomDrinkBtn()
+  }
+  
+
+})
 homeBtn.addEventListener("click", e => {
+  cleanSlate(form)
   HomePageCoctails()
-})
 
 })
 
+//////// FUNCTION FOR A RANDOM DRINK. CLEARS AND CREATES ////////
 
-/// scroll event ////
+ function randomDrinkBtn() {
+ cleanSlate(form)
+  let randomDrinkBtn2 = document.createElement("button")
+  form.append(randomDrinkBtn2)
+  randomDrinkBtn2.innerText = "Get a random Cocktail"
+  randomDrinkBtn2.classList.add("drinkBtn", "drinkBtn2")
+  randomDrinkBtn2.style.width = "300px"
+  
+  
+
+  randomDrinkBtn2.addEventListener("click", e => {
+    cleanSlate(container)
+    getCocktailRandom()
+    if (x.matches) {
+      randomDrinkBtn()
+    }
+  })
+
+}
+
+//////// SCROLL EVENT ////////
+
 window.addEventListener("scroll", () => {
   scrollFunction();
 });
@@ -121,10 +115,13 @@ window.addEventListener("scroll", () => {
 scrollBtn.addEventListener("click", () => {
   differentBrowserFunction();
 });
-///////
+
 
 ///////// FUNCTIONS /////////
 
+
+//// HOMEPAGE FETCH FOR THE SELECTED DRINKS              /////
+/// FETCH FOR RANDOM DRINKS AND SEARCH OPTION FOR DRINKS ////
 function HomePageCoctails() {
   cleanSlate(cocktailDiv)
 
@@ -158,7 +155,7 @@ function getCocktailRandom() {
     .catch(err => {
       console.log(err);
     })
-    cleanSlate(container);
+  cleanSlate(container);
 }
 
 function getCocktailByName(namn) {
@@ -186,7 +183,7 @@ function homepagefetch(namn) {
       }
       return response.json()
     })
-    .then(drink => {      
+    .then(drink => {
       let article = document.createElement("article")
       cocktailDiv.append(article)
       article.classList.add("homePageDrinks")
@@ -203,10 +200,12 @@ function homepagefetch(namn) {
 
 
       article.append(drinkName, img, drinkRecipe)
+      container.append(article)
 
- // event that opens up the entire recipe for the selected drink
-      article.addEventListener("click", () => {        
-        cleanSlate(cocktailDiv)
+      //////// EVENT THAT OPENS UP THE ENTIRE RECIPE FOR THE SELECTED DRINK //////// 
+
+      article.addEventListener("click", () => {
+        cleanSlate(container)
         getACoctail(drink)
       });
 
@@ -214,7 +213,8 @@ function homepagefetch(namn) {
 
 }
 
-///// event som kopplas till fetchen för att skriva ut drink recept 
+//////// FUNCTION TO FETCH INFORMATION AND CREATE ELEMENTS WITH DOM PAST A FOR LOOP  ////////
+
 function getACoctail(drink) {
 
   let article = document.createElement("article")
@@ -231,21 +231,31 @@ function getACoctail(drink) {
 
   article.append(drinkName, img, drinkRecipe)
 
+  //////// FOR LOOP GOES THROUGH 15 I SINCE THE FETCH HAVE 15 DIFFERENT INGRIDIENTS AND 15 MEASURMENTS AS MAX VALUE     ////////
+  //////// CREATE DIV AND SPANS FOR AS LONG AS THE FETCH INFORMATION GIVE US. SO LONG AS THERE IS A INGRIDIENT AND      ////////
+  //////// A MEASURMENT AVALIABLE IT WILL DOM CREATE AND APPEND THEM TO DRINKRECIPE. FINALY PUT EM OUT ON THE WEBSITE   ////////
+
   for (let i = 1; i <= 15; i++) {
     let ingredient = drink.drinks[0]['strIngredient' + i];
+
+
     let measure = drink.drinks[0]['strMeasure' + i];
+
+
+
     if (ingredient && measure) {
       let row = document.createElement('div');
       row.classList.add('row' + i);
       drinkRecipe.appendChild(row);
 
-      let ingredientElement = document.createElement("p");
+      let ingredientElement = document.createElement("span");
       ingredientElement.innerHTML = ingredient;
       drinkRecipe.appendChild(ingredientElement);
 
-      let measureElement = document.createElement("p");
+      let measureElement = document.createElement("span");
       measureElement.innerHTML = measure;
       drinkRecipe.appendChild(measureElement);
+
     }
 
   }
@@ -254,7 +264,52 @@ function getACoctail(drink) {
   drinkRecipe.appendChild(instructions);
 }
 
-///// SCROLL FUNCTION //////
+//////// FUNCTION TO SEARCH FOR A DRINK ////////
+
+function searchForm() {
+
+  let searchInput = document.createElement("input")
+  searchInput.type = "text";
+  searchInput.placeholder = "Mojito... Martini... etc";
+ 
+  let searchDrinkBtn = document.createElement("button")
+  searchDrinkBtn.innerText = "Search Cocktail"
+  searchDrinkBtn.classList.add("drinkBtn")
+
+  form.append(searchInput, searchDrinkBtn)
+
+  searchDrinkBtn.addEventListener("click", e => {
+    e.preventDefault()
+    cleanSlate(container)
+    if (searchInput.value !== "") {
+      getCocktailByName(searchInput.value);
+    }
+    else if (searchInput.value === "") {
+      let article = document.createElement("article")
+      container.append(article)
+
+      let information = document.createElement("h3")
+      information.innerHTML = "You need to search for a drink! If you are too drunk? Don't worry, just press the random drink button ;)"
+      article.append(information)
+
+    }
+    //////// CLEAR THE SITE AND INPUT VALUE   ////////
+
+    searchInput.value = "";
+    searchInput.innerHTML = "";
+
+    ////////  AFTER 9 DRINKS THE USER IS FORWARDED TO A AA METING PAGE  ////////
+
+    timesSearched++;
+    if (timesSearched > 9) {
+      window.location.href = "https://alcoholics-anonymous.eu/meetings/?tsml-region=sweden";
+    }
+
+
+  });
+}
+////////  SCROLL FUNCTION, CREATES A SCROLL ICON. IF scrollBtn   ////////               
+////////    IS FALSE/NOT DEFINED, THE IF STATMENT WILL RUN      ////////
 
 function scrollInitiation() {
   if (!scrollBtn) {
@@ -262,61 +317,65 @@ function scrollInitiation() {
     scrollBtn.id = "scrollBtn";
     scrollBtn.classList.add("scrollbutton");
     container.appendChild(scrollBtn);
-    //scrollBtn.textContent = "⬆"
-    //document.body.style.backgroundImage = " url('https://media.istockphoto.com/id/1462737515/vector/hand-drawn-arrow-up-symbol-funny-concept-with-a-stickman-holding-icon-cartoon-style-design.jpg?s=612x612&w=0&k=20&c=WvsOQ6IMyDBBFojI1S39w59oQ2Nd2BRFRdzo8rxjAAI=')";
-    scrollBtn.style.width = "50px";
-    scrollBtn.style.height = "50px";
-    scrollBtn.style.backgroundSize = "contain";
-    scrollBtn.style.backgroundPosition = "bottom right";
-    scrollBtn.style.backgroundRepeat = "no-repeat";
-    scrollBtn.style.backgroundSize = "50px 50px";
-    scrollBtn.style.display = "none";
+    scrollBtn.textContent = "⬆";
   }
 }
+
+////////   SCROLLBTN WILL EITHER DISPLAY OR NOT. DEPENDS IF              ////////
+////////  THE SCROLLPOSITION IS LARGER THEN 90px. SCROLLTOP IS         ////////
+////////  A BUILT IN, IT MEASURE THE VERTICAL POSITION FOR AN ELEMENT ////////
 
 function scrollFunction() {
   if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
     scrollBtn.style.display = "block"
   } else {
     scrollBtn.style.display = "none";
-  }  
+  }
+  container.appendChild(scrollBtn);
+
 }
+
+////////   TO RESET THE VIEWING DISPLAY FOR THE USER.  ////////
+////////   DIFFERENT BROWSERS HAVE DIFFERENT COMMANDS.  ////////
 
 function differentBrowserFunction() {
   document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;  
+  document.documentElement.scrollTop = 0; 
+  container.appendChild(scrollBtn);  
 }
-///////////////////////////////
- 
 
+////////         MODAL EVENTS.              ////////
 
-
-//modal kod
-
-
-aboutUsBtn.addEventListener("click", e =>{
+aboutUsBtn.addEventListener("click", e => {
   aboutModal.style.display = "block"
 });
 
-closeAboutUs.addEventListener("click", e =>{
+closeAboutUs.addEventListener("click", e => {
   aboutModal.style.display = "none"
 })
 
-window.addEventListener("click", e =>{
-  if(e.target == aboutModal){
+window.addEventListener("click", e => {
+  if (e.target == aboutModal) {
     aboutModal.style.display = "none"
-  }});
+  }
+});
 
-  contactBtn.addEventListener("click", e =>{
-    contactModal.style.display = "block"
-  });
+contactBtn.addEventListener("click", e => {
+  contactModal.style.display = "block"
+});
 
-  closeContact.addEventListener("click", e =>{
+closeContact.addEventListener("click", e => {
+  contactModal.style.display = "none"
+
+});
+
+window.addEventListener("click", e => {
+  if (e.target == contactModal) {
     contactModal.style.display = "none"
+  }
+});
 
-  });
 
- 
 
 
 
